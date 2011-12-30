7803f4e28220c11ce6250fbf28e93cd1411a6e48
Sem <sembrestels@riseup.net>
2011-12-30 04:50:08 +0100

    No more config file. Cofiguration is now set by settings.

--- languages/en.php
+++ languages/en.php
@@ -5,7 +5,24 @@
 
 $english = array(
 	'opensearch:title' => "Search: %s",
-	'opensearch:description' => "Search results for \"%s\"",	
+	'opensearch:description' => "Search results for \"%s\"",
+	'opensearch:engine' => "%s Search Engine",
+	
+	'opensearh:settings:shortname' => "Short name",
+	'opensearh:settings:desc' => "Description",
+	'opensearh:settings:longname' => "Long name",
+	'opensearh:settings:icon' => "Icon",
+	'opensearh:settings:tags' => "Tags",
+	'opensearh:settings:lang' => "Language",
+	'opensearh:settings:query' => "Query",
+	'opensearh:settings:shortname:description' => "Contains a brief human-readable title that identifies this search engine. 16 characters or less of plain text. <strong>Required</strong>",
+	'opensearh:settings:desc:description' => "Contains a human-readable text description of the search engine. 1024 characters or less of plain text. <strong>Required</strong>",
+	'opensearh:settings:longname:description' => "Contains an extended human-readable title that identifies this search engine. 48 characters or less of plain text.",
+	'opensearh:settings:icon:description' => "Contains a URL that identifies the location of an image that can be used in association with this search content. Can be either a 16x16 ico or 64x64 png or both. Clients will choose the image that best fits the display area. The value should be relative to your Elgg root.",
+	'opensearh:settings:tags:description' => "Contains a set of words that are used as keywords to identify and categorize this search content. Tags must be a single word and are delimited by the space character (' '). 256 characters or less, space delimited tags",
+	'opensearh:settings:lang:description' => "Contains a string that indicates that the search engine supports search results in the specified language. * or codes according to XML 1.0 Language Identification",
+	'opensearh:settings:query:description' => "Test query available to clients. Clients can submit this as a test query to ensure that the OpenSearch interface works.",
+	
 );
 
 add_translation("en", $english);
--- views/xml/opensearch/description.php
+++ views/xml/opensearch/description.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Creates an OpenSearch description document from config.ini
+ * Creates an OpenSearch description document from plugin settings
  * http://www.opensearch.org/Specifications/OpenSearch/1.1
  */
 
@@ -8,17 +8,30 @@
 header('Pragma: public', true);
 header('Cache-Control: public', true);
 
-$config_ini = dirname(dirname(dirname(dirname(__FILE__)))) . '/config.ini';
-$config = parse_ini_file($config_ini);
-if ($config == false) {
-	elgg_log("Unable to parse OpenSearch config file", 'ERROR');
-	return true;
-}
-
-extract($config);
-
 $site = elgg_get_site_entity();
 $email = $site->email;
+
+$shortname = elgg_get_plugin_setting('shortname', 'opensearch');
+$description = elgg_get_plugin_setting('desc', 'opensearch');
+$longname = elgg_get_plugin_setting('longname', 'opensearch');
+$icon = elgg_get_plugin_setting('icon', 'opensearch');
+$tags = elgg_get_plugin_setting('tags', 'opensearch');
+$lang = string_to_tag_array(elgg_get_plugin_setting('lang', 'opensearch'));
+$query = elgg_get_plugin_setting('query', 'opensearch');
+
+if (empty($shortname)) {
+	$shortname = $site->name;
+}
+if (empty($description)) {
+	$description = elgg_echo('opensearch:engine', array(elgg_get_site_entity()->name));
+}
+if (empty($icon)) {
+	$icon = '_graphics/favicon.ico';
+}
+if (empty($lang)) {
+	$lang = 'en-us';
+}
+
 
 $rss_url = elgg_normalize_url('search/?q={searchTerms}&view=opensearch_rss');
 $rss_url = elgg_format_url($rss_url);
@@ -35,11 +48,10 @@ $site_url = elgg_get_site_url();
    <Contact><?php echo $site->email; ?></Contact>
    <Url type="text/html" template="<?php echo $html_url; ?>"/>
    <Url type="application/rss+xml" template="<?php echo $rss_url; ?>"/>
-<?php if (isset($ico)): ?>
-   <Image height="16" width="16" type="image/vnd.microsoft.icon"><?php echo "{$site_url}$ico"; ?></Image>
-<?php endif; ?>
-<?php if (isset($png)): ?>
-   <Image height="64" width="64" type="image/png"><?php echo "{$site_url}$png"; ?></Image>
+<?php if (preg_match("/.ico$/", $icon)): ?>
+   <Image height="16" width="16" type="image/vnd.microsoft.icon"><?php echo "{$site_url}$icon"; ?></Image>
+<?php elseif (preg_match("/.png$/", $icon)): ?>
+   <Image height="64" width="64" type="image/png"><?php echo "{$site_url}$icon"; ?></Image>
 <?php endif; ?>
 <?php if (isset($tags)): ?>
    <Tags><?php echo $tags; ?></Tags>
