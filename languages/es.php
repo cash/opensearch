7f6149b755b42125e18b767c7a361132c7a2f8c9
Sem <sembrestels@riseup.net>
2011-12-30 03:46:00 +0100

    Added catalan, spanish, basque and polish languages.

---
+++ languages/es.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * OpenSearch Spanish language file
+ */
+
+$spanish = array( 
+	'opensearch:title'  =>  "Búsqueda: %s" , 
+	'opensearch:description'  =>  "Resultados de la búsqueda \"%s\""
+); 
+
+add_translation('es', $spanish); 
